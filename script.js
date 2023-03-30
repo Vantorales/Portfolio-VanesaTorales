918a5b6df593f17e982334410aea708fa0a418da
Vanesa <vanetorales98@gmail.com>
2023-03-29 23:13:43 -0300

    se arreglaron problemas de estilos, se agregó funcionalidad del formulario

--- script.js
+++ script.js
@@ -49,19 +49,19 @@ window.addEventListener('scroll', () => {
     });
 });
 
-/*===== Boton y función ir arriba =====*/
-window.onscroll = function() {
-    if (document.documentElement.scrollTop > 100) {
-        document.querySelector('.go-top-container').classList.add('show');
-    }
-    else {
-        document.querySelector('.go-top-container').classList.remove('show');
-    }
-}
+// /*===== Boton y función ir arriba =====*/
+// window.onscroll = function() {
+//     if (document.documentElement.scrollTop > 100) {
+//         document.querySelector('.go-top-container').classList.add('show');
+//     }
+//     else {
+//         document.querySelector('.go-top-container').classList.remove('show');
+//     }
+// }
 
-document.querySelector('.go-top-container').addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
+// document.querySelector('.go-top-container').addEventListener('click', () => {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// });
